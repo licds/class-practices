fea6eb5c5ac1eea4004275e76d245ac7f4a80d22
Yi Li <licds@bc.edu>
2020-01-21 17:32:24 -0500

    Initial Commit

---
+++ LunarZodiac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LunarZodiac
+//
+//  Created by Yi Li on 1/21/20.
+//  Copyright © 2020 Yi Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
