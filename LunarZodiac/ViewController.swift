aed0b1409b446c6ffe62cdf3996286c6988d6769
Yi Li <licds@bc.edu>
2020-01-21 19:09:51 -0500

    initial

--- LunarZodiac/ViewController.swift
+++ LunarZodiac/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var yearLabel: UILabel!
+    @IBOutlet weak var yearImages: UIImageView!
+    var startingYear = 2020
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func nextYearButtonPressed(_ sender: UIButton) {
+        startingYear += 1
+        yearLabel.text = String(startingYear)
+        yearImages.image = UIImage(named: "image\((startingYear-4)%12)")
+    }
+    @IBAction func thisYearButtonPressed(_ sender: UIButton) {
+        startingYear -= 1
+        yearLabel.text = String(startingYear)
+        yearImages.image = UIImage(named: "image\((startingYear-4)%12)")
+    }
+    
 
 }
 
